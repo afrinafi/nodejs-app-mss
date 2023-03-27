71d210e759ba145bdcc48c6d6a7ea19772ffd9a4
afrinafi <113271272+afrinafi@users.noreply.github.com>
2023-03-27 03:02:35 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.221.74.112:9000/',
+  serverUrl: 'http://44.201.54.156:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.projectName': 'Nodejs-Application-example',
+	    'sonar.projectKey':'Nodejs-Application-example',
+	    'sonar.login': 'sqp_6a0f0d3f4a56f740c3ac7b017816552fab01def3',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
